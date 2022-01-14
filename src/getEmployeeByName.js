d98fb2c450034b1a06d188bc5ae4ee6df23d7bd3
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-13 22:13:41 -0300

    Requisito 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,7 +4,7 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
     return {};
-  } // caso a função não tenha entrada, retora um objeto vazio
+  } // caso a função não tenha entrada, retorna um objeto vazio
   const emp = employees.find((employ) =>
     employ.firstName === employeeName || employ.lastName === employeeName);
   return emp; // procura os empregados com o primeiro ou o último nome e retorna todos os dados/objeto do mesmo!
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employ) => employ.managers.includes(id)); // retorna true se o id passado é de um gerente // seu código aqui
+  // console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const objEmp = employees.filter((man) => man.managers.includes(managerId));
+    return objEmp.map((employ) => `${employ.firstName} ${employ.lastName}`);// Cria um novo array de string com as mesmas quantidades dos gerentes existentes // seu código aqui
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
